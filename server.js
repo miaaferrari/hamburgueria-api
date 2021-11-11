abac25fce0b81b4bdfd666e45b1348843aee0029
Mia Ferrari <miaa_ferrari@hotmail.com>
2021-11-10 22:18:36 -0300

    initial commit

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.db = router.db;
 
 const rules = auth.rewriter({
   users: 600,
+  products: 664, //everyone can read
+  preferences: 660, //user must be logged to write or read
 });
 
 app.use(cors());
